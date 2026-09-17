2b11b6b7519ee322f5dae618bdb1c4d3d86c5336
Haydwn <haydenhaygood@gmail.com>
2026-09-16 21:56:13 -0500

    Added OFFICER_ROLE enum

---
+++ Officer_role.h
@@ -0,0 +1,17 @@
+#ifndef OFFICER_ROLE_H
+#define OFFICER_ROLE_H
+
+namespace club {
+
+    enum OFFICER_ROLE {
+        PRESIDENT,
+        VICE_PRESIDENT,
+        TREASURER,
+        SECRETARY,
+        EVENTS_CHAIR,
+        INTERNET_CHAIR
+    };
+
+}
+
+#endif // OFFICER_ROLE_H
