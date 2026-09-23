8b3fb79d95932a89cb99e3325f2fd654bdfed8c5
Haydwn <haydenhaygood@gmail.com>
2026-09-23 00:40:53 -0500

    edited Screen

--- Screen.cpp
+++ Screen.cpp
@@ -8,14 +8,14 @@ Screen::Screen(std::string screen_title,
 			vector<std::pair<char, std::string>> menu_options)
 {
 	Menu.set_title(screen_title);
-	for (auto & option : menu_options) {
+	for (const auto& option : menu_options) {
 		Menu.add_item(option.first, option.second);
 	}
 }
 
 void Screen::show()
 {
-	for (auto &option : Menu) {
+	for (const auto& option : Menu) {
 		std::cout << option << std::endl;
 	}
 }
